a7672fc846745987000f12b3dd9270e1ad7d7a74
falkTX <falktx@falktx.com>
2021-03-24 12:05:40 +0000

    Manually specify nand id and properties for MT29F32G08CBACAWP
    Signed-off-by: falkTX <falktx@falktx.com>

--- drivers/mtd/nand/raw/nand_ids.c
+++ drivers/mtd/nand/raw/nand_ids.c
@@ -58,6 +58,9 @@ struct nand_flash_dev nand_flash_ids[] = {
 	{"TH58NVG2S3HBAI4 4G 3.3V 8-bit",
 		{ .id = {0x98, 0xdc, 0x91, 0x15, 0x76} },
 		  SZ_2K, SZ_512, SZ_128K, 0, 5, 128, NAND_ECC_INFO(8, SZ_512) },
+	{"MT29F32G08CBACAWP",
+		{ .id = {0x2c, 0x68, 0x4, 0x4a, 0xa9, 0x0} },
+		  SZ_4K, SZ_4K, SZ_1M, NAND_NEED_SCRAMBLING, 6, 224, NAND_ECC_INFO(24, SZ_1K) },
 
 	LEGACY_ID_NAND("NAND 4MiB 5V 8-bit",   0x6B, 4, SZ_8K, SP_OPTIONS),
 	LEGACY_ID_NAND("NAND 4MiB 3,3V 8-bit", 0xE3, 4, SZ_8K, SP_OPTIONS),
